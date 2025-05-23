2f55b7c6878ed4e7a157fd9582d3bf0c3f696f4e
SarahGodfrey51574 <sara0009@student.monash.edu>
2025-05-23 12:06:29 +1000

    upload

---
+++ class-changes.js
@@ -0,0 +1,14 @@
+const classChanges = [
+  { time: 1, target: ".loading", remove: "visible" },
+  { time: 7, target: ".", add: "" },
+  { time: 9, target: ".", add: "" },
+  { time: 12, target: ".", remove: "" },
+
+  { time: 9, target: ".changeme2", add: "show" },
+  { time: 16, target: ".changeme2", remove: "show" },
+  { time: 16, target: ".changeme2", add: "hidden" },
+
+  { time: 16, target: ".changeme3", add: "show" },
+  { time: 28, target: ".changeme3", remove: "show" },
+  { time: 28, target: ".changeme3", add: "hidden" }
+];
